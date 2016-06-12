bd2c85e04d1a9cbb6d8043f3360cf0d9d8fbdbbe
Rajith Sanjeewa <rajiththennakoonshady@gmail.com>
2016-06-12 17:57:53 +0530

    icons ans some changes

--- settings.php
+++ settings.php
@@ -36,9 +36,11 @@ if(logged_in() === false){
 
             <label for="email"> <?php echo $user_data['first_name']; ?>'s Profile </label>
 
-            <a href="logout.php"><input type="image" src="images/icons/logout.png" name="logout" value="Sign Out" style="margin-left:10px;font-weight:bold;"/></a>
 
-            <a href="settings.php"><input type="image" src="images/icons/user.png" name="setting" value="settings " style="margin-left:10px;font-weight:bold;"/></a>
+            <a href="<?php echo $user_data['first_name'];?>"><input type="image" src="images/icons/user.png" name="setting" value="settings " style="margin-left:10px;"/></a>
+            <input type="image" src="images/icons/settings.png" style="margin-left:10px;">
+            <a href="logout.php"><input type="image" src="images/icons/logout.png" name="logout" value="Sign Out" style="margin-left:10px;"/></a>
+
 
 
 
