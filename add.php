6a0f07c85a2637632b4df41e639ecdbd315afb73
Rajith Sanjeewa <rajiththennakoonshady@gmail.com>
2016-06-17 20:48:18 +0530

    profile page fixed

--- add.php
+++ add.php
@@ -33,7 +33,8 @@ if(logged_in() === false){
             <nav>
 
 
-                <label for="email">Welcome <?php echo $user_data['first_name']; ?> </label>
+                <label for="email"><?php echo $user_data['first_name']; ?> </label>
+                <a href="home.php"><input type="image" src="images/icons/home.png" title="Home" value="home " style="margin-left:10px;"/></a>
                 <a href="<?php echo $user_data['first_name'];?>"><input type="image" src="images/icons/user.png" name="setting" value="settings " style="margin-left:10px;font-weight:bold;"/></a>
                 <a href="logout.php"><input type="image" src="images/icons/logout.png" name="logout" value="Sign Out" style="margin-left:10px;font-weight:bold;"/></a>
 
