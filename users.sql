004eefbe1f90b668b1f000ca29fc656385dd51f5
rajithst <rajiththennakoonshady@gmail.com>
2021-08-08 12:09:50 +0900

    add updated sql file

--- users.sql
+++ users.sql
@@ -1,20 +1,21 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 06, 2016 at 06:15 PM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
+-- Host: localhost:8889
+-- Generation Time: Aug 08, 2021 at 03:08 AM
+-- Server version: 5.7.32
+-- PHP Version: 7.4.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `assets`
@@ -23,45 +24,81 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
---
-
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(32) NOT NULL,
-  `last_name` varchar(32) NOT NULL,
-  `email` varchar(32) NOT NULL,
-  `password` varchar(32) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
-
---
 -- Table structure for table `assets`
 --
 
-CREATE TABLE IF NOT EXISTS assets (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  userid int(11) NOT NULL,
-  title varchar(32) NOT NULL,
-  category varchar(32) NOT NULL,
-  quantity int(11) NOT NULL,
-  price FLOAT NOT NULL,
-  details VARCHAR(1000),
-  PRIMARY KEY(id)
-);
+CREATE TABLE `assets` (
+  `id` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `title` varchar(32) NOT NULL,
+  `category` varchar(32) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `price` float NOT NULL,
+  `details` varchar(1000) DEFAULT NULL,
+  `status` int(11) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `assets`
+--
+
+INSERT INTO `assets` (`id`, `userid`, `title`, `category`, `quantity`, `price`, `details`, `status`) VALUES
+(4, 1, 'mychair', 'chair', 1, 22, '222', 1),
+(6, 1, 'mychair', 'chair', 1, 1500, 'wqqw', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(32) NOT NULL,
+  `last_name` varchar(32) NOT NULL,
+  `email` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`first_name`, `last_name`, `email`, `password`) VALUES
-('s', 'ds', 'rajith', 'rajith'),
-('rajti', 'da', 'rajith', 'rajith'),
-('x', 'x', 'rajith', 'rajith'),
-('gd', 'fd', 'rajith', 'rajith'),
-('da', 'd', 'rajith', 'rajith'),
-('rajith', 'thennakoon', 'rajiththennakoonsahdy@gmail.com', 'rajith'),
-('rajith', 'ds', 'rajith', 'rajith');
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`) VALUES
+(1, 'Rajith', 'Thennakoon', 'rajithsh@gmail.com', 'f777215f640fad06c016944391fceb46');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `assets`
+--
+ALTER TABLE `assets`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `assets`
+--
+ALTER TABLE `assets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
